b4624237dcfe391b20f389fa35a0c1dff9dc0d01
SayanInfom <sayan.mukherjee@infomaticae.co.in>
2025-04-27 14:25:13 +0530

    lazy load 3

---
+++ src/tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      './src/**/*.{js,jsx,ts,tsx}',
+      './public/index.html',
+    ],
+    safelist: [
+      'lazy-zoom',
+      'zoom-in-visible',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
